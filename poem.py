26819509878e6f874f565683fc743aa45904eada
tatum <tatumdv2001@gmail.com>
2020-09-25 15:31:45 +0200

    poem

---
+++ poem.py
@@ -0,0 +1,21 @@
+vers1 = "Ik ben Tatum, mensen vragen mij vaak de datum."
+vers2 = "Maar ik woon dan ook in Schagen"
+vers3 = "Over die stad zul jij mij vaak horen klagen"
+vers4 = "Vandaar dat ik het liefst ook wil slapen, en dan ook veel zou gapen"
+vers5 = "Dan moet ik denken aan schapen"
+vers6 = "Al heeft het er niks mee te maken"
+vers7 = "Daar zul je mij wel wat over kunnen vragen"
+vers8 = "Dat zijn de games die ik speel waar ik soms ook bij gil"
+vers9 = "Laat dat nu maar zitten voordat ik u tijd verder verspil"
+vers10 = "goodbye it was nice, see you next time!"
+
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)
+print(vers5)
+print(vers6)
+print(vers7)
+print(vers8)
+print(vers9)
+print(vers10)
