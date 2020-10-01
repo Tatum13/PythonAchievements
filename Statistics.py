f057bad35ff139775186f0fc1119a111d56c6c93
tatum <tatumdv2001@gmail.com>
2020-10-01 15:33:13 +0200

    statistics

---
+++ Statistics.py
@@ -0,0 +1,20 @@
+naam = "Tatum"
+leeftijd = 18
+geboorteDatum = "geboren 13-10-2001"
+haarkleur = "heeft rood haar: "
+haarkleur2 = True
+hobby1 = "hobbies:gamen - "
+hobby2 = "muziek luisteren - "
+hobby3 = "series kijken"
+huisdier = "huisdier: hond"
+favorietegames = "favoriete games: Ark survival, call of duty, enzovoort"
+verzameld = "verzameld: funko pops"
+
+print(naam)
+print(leeftijd)
+print(geboorteDatum)
+print(haarkleur + str(haarkleur2))
+print(hobby1 + hobby2 + hobby3)
+print(huisdier)
+print(favorietegames)
+print(verzameld)
