e53242d966601818671b6069445244217590b920
tatum <tatumdv2001@gmail.com>
2020-10-02 14:33:00 +0200

    how you like apples

---
+++ appels.py
@@ -0,0 +1,56 @@
+#opdracht
+
+print("in een boomgaard staan 333 appelbomen")
+print("2/3 deel van de bomen staat in de schaduw van een berg")
+print("en geeft hierdoor maar 80% van de appels in vergelijking met de bomen die in de zon staan")
+print("een boom die in de zon staat geeft precies 146 appels")
+print("de boomgaard is van mij en 3 vrienden en we verdelen alle appels eerlijk.")
+
+print("-----------------------------------------------------------------------------------------------------")
+
+#aftrekken
+print("333 bomen delen door 3 x 2:")
+print("(333 : 3) x 2 =")
+bomen = 333
+bomen /= 3
+bomen *= 2
+print(bomen)
+print("dus 222 bomen")
+
+print("-----------------------------------------------------------------------------------------------------")
+#bomen in de zon geven 146 appels per boom (111 bomen)
+print("per boom dat in de schaduw staat dat appels geeft: ")
+print("(222 x 146) : 100 x 80 =")
+bomen2 = 222
+bomen2 *= 146
+bomen2 /= 100
+bomen2 *= 80
+print(bomen2)
+print("dus 25930 appels")
+#25930 appels
+print("-----------------------------------------------------------------------------------------------------")
+
+print("bomen in de zon hebben 111 x 146 appels dus:")
+appels2 = 111
+appels2 *= 111
+print(appels2)
+print("dus 12321 appels")
+#12321 apples
+
+print("-----------------------------------------------------------------------------------------------------")
+
+print("aantal appels in schaduw + aantal appels in de zon = ")
+print("25930 + 12321 = ")
+appels3 = 25930
+appels3 += 12321
+print(appels3)
+print("dus in totaal 38251 appels ")
+
+print("-----------------------------------------------------------------------------------------------------")
+
+print("moet verdeeld worden over 4 mensen dus aantal appels : 4")
+print("38251 : 4 =")
+appels4 = 38251
+appels4 /= 4
+print(appels4)
+print("dus ongeveer 9563 appels per persoon")
