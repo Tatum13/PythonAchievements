6ad15e5545374a70101df6ae87544cbf0a135dd3
tatum <tatumdv2001@gmail.com>
2020-10-02 13:26:07 +0200

    my first operations

---
+++ myfirstOperations.py
@@ -0,0 +1,32 @@
+#toekennen
+x = 9
+y = 5
+t = 8
+p = 12
+k = 40
+L = 2
+
+#optellen
+x += 5
+print(x)
+
+#aftrekken
+y -= 2
+print(y)
+
+#keer
+t *= 2
+print(t)
+
+#delen en dubbel delen
+p /= 2
+p //= 2
+print(p)
+
+#restwaarde
+k %= 15
+print(k)
+
+#tot de macht
+L **= 2
+print(L)
