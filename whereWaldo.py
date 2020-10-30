8620e4a00cdee9c76a692ac92366a4cc4bb444b5
tatum <tatumdv2001@gmail.com>
2020-10-30 13:02:25 +0100

    Where is waldo

--- whereWaldo.py
+++ whereWaldo.py
@@ -2,10 +2,13 @@ import random
 people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
 random.shuffle(people)
 
+
+
+x = "Waldo"
+stoel = 0
 print(people)
 
-for x in people:
-    if x == "Waldo":
-        stoel = people.index(x)
-        print(stoel)
-        break
+for y in people:
+    stoel += 1
+    if y == "Waldo":
+        print("Waldo zit op stoel nummer:" + str(stoel))
